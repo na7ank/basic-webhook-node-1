621429dc92861835e1cf1d373090d2fb2168dd76
Natan Bronzatto <96526051+na7ank@users.noreply.github.com>
2024-08-09 15:25:22 +0000

    update.

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
 
+
 // Middleware para analisar o corpo da requisição como JSON
 app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World !');
-});
-
-app.get('/oi', (req, res) => {
-  res.send('Olá Mundo !');
-});
 
 // Novo endpoint POST para receber JSON e exibir na tela
 app.post('/json', (req, res) => {
