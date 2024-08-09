6fce09805397a39028e53b558c081d1b7543c7e0
Natan Bronzatto <96526051+na7ank@users.noreply.github.com>
2024-08-09 15:18:49 +0000

    update.

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-// index.js
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+// Middleware para analisar o corpo da requisição como JSON
+app.use(express.json());
 
 app.get('/', function (req, res) {
-  res.send('Hello World !')
-})
+  res.send('Hello World !');
+});
 
-// index.js
 app.get('/oi', (req, res) => {
-    res.send('Olá Mundo !')
-})
+  res.send('Olá Mundo !');
+});
 
 // Novo endpoint POST para receber JSON e exibir na tela
 app.post('/json', (req, res) => {
-    // Obtém o corpo da requisição
-    const jsonData = req.body;
-    const key = req.header.key;
-
-    
-    // Exibe o JSON recebido na tela
-    console.log(jsonData)
-    res.json({
-      message: 'JSON recebido!',
-      data: jsonData
-    });
-  });
+  // Obtém o corpo da requisição
+  const jsonData = req.body;
   
+  // Exibe o JSON recebido na tela
+  console.log(jsonData);
+  res.json({
+    message: 'JSON recebido!',
+    data: jsonData
+  });
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Servidor ouvindo na porta 3000');
+});
